16dba157c1e2ca6086714a68ca4066260537a7a1
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-01-04 21:53:48 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+function App() {
+	return (
+		<div className="App">
+			hi ther <Button type="primary">Primary Button</Button>
+		</div>
+	);
+}
+
+export default App;
