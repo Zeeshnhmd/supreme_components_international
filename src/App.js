ead9357921bd9aaab78db656ef1792216e32184d
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-01-05 02:19:48 +0530

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { Button } from 'antd';
+import { Route, Routes } from 'react-router-dom';
+
 import Layout from './components/layout';
+import Home from './pages/home';
+import Insights from './pages/my-sales-peformance/insights';
+import Trends from './pages/my-sales-peformance/trends';
 
 function App() {
 	return (
-		<>
-			<Layout />
-		</>
+		<Layout>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sales-performance/insights" element={<Insights />} />
+				<Route path="/sales-performance/trends" element={<Trends />} />
+			</Routes>
+		</Layout>
 	);
 }
 
