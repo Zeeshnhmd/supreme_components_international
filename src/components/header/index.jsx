e3b5b4b1ac47410c880a0a0dd049b2802f69c6aa
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-01-05 07:27:08 +0530

    feat: header color change logic

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,17 @@
-import { QuestionCircleOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Dropdown } from 'antd';
-import React from 'react';
+import {
+	EyeOutlined,
+	QuestionCircleOutlined,
+	SettingOutlined,
+} from '@ant-design/icons';
+import { Avatar, Dropdown, Tag } from 'antd';
+import React, { useState } from 'react';
 import styles from './header.module.scss';
 
-const Header = () => {
+const Header = ({ userTypes }) => {
+	console.log(userTypes);
+
+	const [userType, setUserType] = useState('SR');
+
 	const items = [
 		{
 			label: <a href="/">View Profile</a>,
@@ -16,7 +24,16 @@ const Header = () => {
 	];
 
 	return (
-		<div className={styles['header-wrapper']}>
+		<div
+			className={`${styles['header-wrapper']}
+			 ${userType === 'SR' && styles['header-sr']}
+			 ${userType === 'SM' && styles['header-sm']}
+			 ${userType === 'CE' && styles['header-ce']}
+			 ${userType === 'S' && styles['header-s']}
+			 ${userType === 'Admin' && styles['header-admin']}
+			 ${!userType && styles['no-user-type']}
+			 `}
+		>
 			<div className={styles['user-name']}>
 				<p className={styles['name']}>Hello, Aslam</p>
 			</div>
@@ -24,6 +41,20 @@ const Header = () => {
 				<p className={styles['brand-name']}>Welcome to Eden</p>
 			</div>
 			<div className={styles['users-wrapper']}>
+				<div className={styles['view-type']}>
+					<EyeOutlined /> <p className={styles['user-type']}>{userType}</p>
+				</div>
+				<div>
+					{userTypes.map((el) => (
+						<Tag
+							className={styles['tags']}
+							key={el?.id}
+							onClick={() => setUserType(el?.type)}
+						>
+							<p className={styles['tag-name']}> {el?.type}</p>
+						</Tag>
+					))}
+				</div>
 				<SettingOutlined className={styles['nav-options']} />
 				<QuestionCircleOutlined className={styles['nav-options']} />
 				<Dropdown
