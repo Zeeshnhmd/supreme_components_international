3a9b8dd6d0d99ca8a873be6bbd210fd9aedef73c
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-01-11 22:16:15 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/layout';
 import Journey from './pages/home/journey';
 import Insights from './pages/my-sales-peformance/insights';
 
+import './styles/index.scss';
 
 function App() {
 	const userTypes = [
@@ -21,31 +22,83 @@ function App() {
 		<Layout userTypes={userTypes} userType={userType} setUserType={setUserType}>
 			<Routes>
 				<Route path="/" element={<Journey userType={userType} />} />
-				<Route path="/my-journey" element={<Journey userType={userType}  />} k />
+				<Route path="/my-journey" element={<Journey userType={userType} />} k />
 
-				<Route  path="/sales-performance" element={<h1>Sales Performance</h1>}  />
+				<Route
+					path="/sales-performance"
+					element={<h1 className="title">Sales Performance</h1>}
+				/>
 				<Route path="/sales-performance/insights" element={<Insights />} />
-				<Route path="/sales-performance/trends" element={<h1>Trends</h1>} />
-				<Route path="/sales-performance/customer-level" element={<h1>Customer Level</h1>} />
-				<Route path="/sales-performance/mnp-level" element={<h1>MNP Level</h1>} />
+				<Route
+					path="/sales-performance/trends"
+					element={<h1 className="title">Trends</h1>}
+				/>
+				<Route
+					path="/sales-performance/customer-level"
+					element={<h1 className="title">Customer Level</h1>}
+				/>
+				<Route
+					path="/sales-performance/mnp-level"
+					element={<h1 className="title">MNP Level</h1>}
+				/>
 
-				<Route path="/email-engagement" element={<h1>Email Engagement</h1>} />
-				<Route path="/email-engagement/customer-level" element={<h1>email-engagement/customer-level</h1>} />
-				<Route path="/email-engagement/contact-level" element={<h1>email-engagement/contact-level</h1>} />
-				<Route path="/email-engagement/conversation-statics" element={<h1>email-engagement/conversation-staticsl</h1>} />
+				<Route
+					path="/email-engagement"
+					element={<h1 className="title">Email Engagement</h1>}
+				/>
+				<Route
+					path="/email-engagement/customer-level"
+					element={<h1 className="title">email-engagement/customer-level</h1>}
+				/>
+				<Route
+					path="/email-engagement/contact-level"
+					element={<h1 className="title">email-engagement/contact-level</h1>}
+				/>
+				<Route
+					path="/email-engagement/conversation-statics"
+					element={
+						<h1 className="title">email-engagement/conversation-staticsl</h1>
+					}
+				/>
 
+				<Route
+					path="/rfq-opportunities"
+					element={<h1 className="title">/rfq-opportunities</h1>}
+				/>
+				<Route
+					path="/rfq-opportunities/rfq-stats"
+					element={<h1 className="title">/rfq-opportunities/rfq-stats</h1>}
+				/>
+				<Route
+					path="/rfq-opportunities/rfq-accounts"
+					element={<h1 className="title">/rfq-opportunities/rfq-accounts</h1>}
+				/>
 
-				<Route path="/rfq-opportunities" element={<h1>/rfq-opportunities</h1>} />
-				<Route path="/rfq-opportunities/rfq-stats" element={<h1>/rfq-opportunities/rfq-stats</h1>} />
-				<Route path="/rfq-opportunities/rfq-accounts" element={<h1>/rfq-opportunities/rfq-accounts</h1>} />
+				<Route
+					path="/my-sources"
+					element={<h1 className="title">/my-sources</h1>}
+				/>
+				<Route
+					path="/my-sources/mpn-queue"
+					element={<h1 className="title">/my-sources/mpn-queues</h1>}
+				/>
+				<Route
+					path="/my-sources/mnp-cost-book"
+					element={<h1 className="title">/my-sources/mnp-cost-book</h1>}
+				/>
 
-				<Route path="/my-sources" element={<h1>/my-sources</h1>} />
-				<Route path="/my-sources/mpn-queue" element={<h1>/my-sources/mpn-queues</h1>} />
-				<Route path="/my-sources/mnp-cost-book" element={<h1>/my-sources/mnp-cost-book</h1>} />
-
-				<Route path="/my-quotes" element={<h1>/my-quotes</h1>} />
-				<Route path="/my-quotes/quote-summary" element={<h1>/my-quotes/quote-summary</h1>} />
-				<Route path="/my-quotes/mnp-quote-book" element={<h1>/my-quotes/mnp-quote-book</h1>} />
+				<Route
+					path="/my-quotes"
+					element={<h1 className="title">/my-quotes</h1>}
+				/>
+				<Route
+					path="/my-quotes/quote-summary"
+					element={<h1 className="title">/my-quotes/quote-summary</h1>}
+				/>
+				<Route
+					path="/my-quotes/mnp-quote-book"
+					element={<h1 className="title">/my-quotes/mnp-quote-book</h1>}
+				/>
 			</Routes>
 		</Layout>
 	);
